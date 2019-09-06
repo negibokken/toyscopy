dec5633abfa12be1fe084e412a1a4f4acd077ec6
negibokken <negibokken@gmail.com>
2019-09-06 23:06:28 +0900

    Update files

--- src/flash/html/html_document_parser.cc
+++ src/flash/html/html_document_parser.cc
@@ -1,3 +1,12 @@
 #include "./html_document_parser.h"
+#include <iostream>
 
-void HTMLDocumentParser::parse() { printf("hello world\n"); }
+void HTMLDocumentParser::parse(std::string str)
+{
+  std::cout << "hello html document parser" << std::endl;
+}
+
+void HTMLDocumentParser::nextToken()
+{
+  std::cout << "hello next Token is here" << std::endl;
+}
--- src/flash/html/html_document_parser.h
+++ src/flash/html/html_document_parser.h
@@ -2,9 +2,12 @@
 #define HTML_DOCUMENT_PARSER
 
 #include <stdio.h>
+#include <string>
+
 class HTMLDocumentParser {
   public:
   HTMLDocumentParser() {}
-  void parse();
+  void parse(std::string str);
+  void nextToken();
 };
 #endif
