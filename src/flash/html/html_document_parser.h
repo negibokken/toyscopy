4b96f6f57b4b8635df9e21b86863159f710cf753
negibokken <negibokken@gmail.com>
2019-09-18 13:56:34 +0900

    Add html parser state

--- src/flash/html/html_document_parser.h
+++ src/flash/html/html_document_parser.h
@@ -8,7 +8,9 @@
 class HTMLDocumentParser {
   public:
   enum State {
-
+    initial, before_html, before_head, in_head, in_head_noscript, after_head, in_body, text,
+    in_table, in_table_text, in_caption, in_column_group, in_table_body, in_row, in_cell, in_select,
+    in_template, after_body, in_frameset, after_frameset, after_after_body, after_after_frameset
   };
   HTMLDocumentParser() {}
   void parse(std::string str);
