a7b841025a58f158b0ba30ea716b6b7025a0e90d
negibokken <negibokken@gmail.com>
2019-11-23 23:35:57 +0900

    Add tool bar on the top of browser

--- src/core/main.cc
+++ src/core/main.cc
@@ -4,18 +4,37 @@
 #include <gtk/gtk.h>
 #include <stack>
 
+static void attachHeaderBar(GtkWidget *window){
+  GtkWidget *header_bar;
+  header_bar = gtk_header_bar_new();
+  gtk_header_bar_set_show_close_button (GTK_HEADER_BAR(header_bar), true);
+
+  GtkWidget *search_bar;
+  search_bar = gtk_entry_new();
+  gtk_header_bar_pack_end(GTK_HEADER_BAR(header_bar), search_bar);
+  gtk_window_set_titlebar(GTK_WINDOW(window), header_bar);
+
+  gtk_widget_show_all(window);
+}
+
 static void activate(GtkApplication *app, gpointer user_data)
 {
+
   GtkWidget *window;
   GtkWidget *button;
   GtkWidget *button_box;
 
   window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "ToyScopy");
+  gtk_window_set_title(GTK_WINDOW(window), "toyscopy");
   gtk_window_set_default_size(GTK_WINDOW(window), 480, 480);
   gtk_window_set_resizable(GTK_WINDOW(window), true);
   gtk_window_set_gravity(GTK_WINDOW(window), GDK_GRAVITY_SOUTH_EAST);
 
+  attachHeaderBar(window);
+
+  gtk_widget_show_all(window);
+
+    // Call HTML Renderer
   HTMLDocumentParser *hdp = new HTMLDocumentParser("<html>Hello World</html>");
   hdp->parse();
 
