a1b04a158dfaffcbda59b020c9af2f105f034324
negibokken <negibokken@gmail.com>
2020-08-13 23:33:00 +0900

    Change case statement to if else

--- src/flash/css/css_tokenizer.cc
+++ src/flash/css/css_tokenizer.cc
@@ -65,154 +65,143 @@ void CSSTokenizer::pumpToken() {
 
   char c = nextInputCharacter();
 
-  switch (c) {
-    // TODO: consume comments
-    // whitespace
-    case ' ': {
-      // Consume as much whitespace as possible
-      while (isNext(' '))
-        nextInputCharacter();
-      CSSToken* token = createCSSToken(CSSToken::CSSTokenType::WhitespaceToken);
-      emitToken(token);
-      return;
-    }
-    // U+0022 QUOTATION MARK (")
-    case '"': {
-      CSSToken* token = createCSSToken(CSSToken::CSSTokenType::StringToken);
-      while (hasNextCharacter()) {
-        c = nextInputCharacter();
-        // Consume a string token
-        if (c == '"') {
-          emitToken(token);
-          return;
-        } else if (c == '\0') {
-          // This is a parse error.
-          emitToken(token);
-          return;
-        } else if (c == '\n') {
-          // This is a parse error.
-          token = createCSSToken(CSSToken::CSSTokenType::BadstringToken);
-          return;
-        } else if (c == '\\') {
-          // TODO:
-          // if EOF do nothing
-          // if newline consume it
-          // if valid escape append codepoint
-        } else {
-          token->appendValue(c);
-        }
-      }
-      return;
-    }
-    // U+0023 NUMBER SIGN
-    case '#': {
-      break;
-    }
-    // U+0027 APOSTROPHE
-    case '\'': {
-      CSSToken* token = createCSSToken(CSSToken::CSSTokenType::StringToken);
-      while (hasNextCharacter()) {
-        c = nextInputCharacter();
-        // Consume a string token
-        if (c == '"') {
-          emitToken(token);
-          return;
-        } else if (c == '\0') {
-          // This is a parse error.
-          emitToken(token);
-          return;
-        } else if (c == '\n') {
-          // This is a parse error.
-          token = createCSSToken(CSSToken::CSSTokenType::BadstringToken);
-          return;
-        } else if (c == '\\') {
-          // TODO:
-          // if EOF do nothing
-          // if newline consume it
-          // if valid escape append codepoint
-        } else {
-          token->appendValue(c);
-        }
-      }
-      return;
-    }
-    // U+0028 LEFT PARENTHESIS(()
-    case '(': {
-      break;
-    }
-    // U+0029 RIGHT PARENTHESIS())
-    case ')': {
-      break;
-    }
-    // U+002B PLUS SIGN (+)
-    case '+': {
-      break;
-    }
-    // U+002C COMMA (,)
-    case ',': {
-      break;
-    }
-    // U+002D HYPHEN-MINUS (-)
-    case '-': {
-      break;
-    }
-    // U+002E FULL STOP (.)
-    case '.': {
-      break;
-    }
-    // U+003A COLON (:)
-    case ':': {
-      break;
-    }
-    // U+003B SEMI COLON (;)
-    case ';': {
-      break;
-    }
-    // U+003C LESS-THAN SIGN (<)
-    case '<': {
-      break;
-    }
-    // U+0040 COMMERCIAL AT (@)
-    case '@': {
-      break;
-    }
-    // U+005B LEFT SQUARE BRACKET ([)
-    case '[': {
-      break;
-    }
-    // U+005D RIGHT SQUARE BRACKET (])
-    case ']': {
-      break;
-    }
-    // U+007B LEFT CURLY BRACKET ({)
-    case '{': {
-      break;
-    }
-    // U+007D RIGHT CURLY BRACKET (})
-    case '}': {
-      break;
-    }
-    // digit
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9': {
-      break;
-    }
-    // EOF
-    case '\0': {
-      break;
-    }
-    // delim
-    default: {
-    }
+  // TODO: consume comments
+  // whitespace
+  if (c == ' ') {
+    // Consume as much whitespace as possible
+    while (isNext(' '))
+      nextInputCharacter();
+    CSSToken* token = createCSSToken(CSSToken::CSSTokenType::WhitespaceToken);
+    emitToken(token);
+    return;
   }
-}
+  // U+0022 QUOTATION MARK (")
+  else if (c == '"') {
+    CSSToken* token = createCSSToken(CSSToken::CSSTokenType::StringToken);
+    while (hasNextCharacter()) {
+      c = nextInputCharacter();
+      // Consume a string token
+      if (c == '"') {
+        emitToken(token);
+        return;
+      } else if (c == '\0') {
+        // This is a parse error.
+        emitToken(token);
+        return;
+      } else if (c == '\n') {
+        // This is a parse error.
+        token = createCSSToken(CSSToken::CSSTokenType::BadstringToken);
+        return;
+      } else if (c == '\\') {
+        // TODO:
+        // if EOF do nothing
+        // if newline consume it
+        // if valid escape append codepoint
+      } else {
+        token->appendValue(c);
+      }
+    }
+    return;
+  }
+  // U+0023 NUMBER SIGN
+  else if (c == '#') {
+    return;
+  }
+  // U+0027 APOSTROPHE
+  else if (c == '\'') {
+    CSSToken* token = createCSSToken(CSSToken::CSSTokenType::StringToken);
+    while (hasNextCharacter()) {
+      c = nextInputCharacter();
+      // Consume a string token
+      if (c == '"') {
+        emitToken(token);
+        return;
+      } else if (c == '\0') {
+        // This is a parse error.
+        emitToken(token);
+        return;
+      } else if (c == '\n') {
+        // This is a parse error.
+        token = createCSSToken(CSSToken::CSSTokenType::BadstringToken);
+        return;
+      } else if (c == '\\') {
+        // TODO:
+        // if EOF do nothing
+        // if newline consume it
+        // if valid escape append codepoint
+      } else {
+        token->appendValue(c);
+      }
+    }
+    return;
+  }
+  // U+0028 LEFT PARENTHESIS(()
+  else if ('(') {
+    return;
+  }
+  // U+0029 RIGHT PARENTHESIS())
+  else if (')') {
+    return;
+  }
+  // U+002B PLUS SIGN (+)
+  else if ('+') {
+    return;
+  }
+  // U+002C COMMA (,)
+  else if (',') {
+    return;
+  }
+  // U+002D HYPHEN-MINUS (-)
+  else if ('-') {
+    return;
+  }
+  // U+002E FULL STOP (.)
+  else if ('.') {
+    return;
+  }
+  // U+003A COLON ())
+  else if (')') {
+    return;
+  }
+  // U+003B SEMI COLON (;)
+  else if (';') {
+    return;
+  }
+  // U+003C LESS-THAN SIGN (<)
+  else if ('<') {
+    return;
+  }
+  // U+0040 COMMERCIAL AT (@)
+  else if ('@') {
+    return;
+  }
+  // U+005B LEFT SQUARE BRACKET ([)
+  else if ('[') {
+    return;
+  }
+  // U+005D RIGHT SQUARE BRACKET (])
+  else if (']') {
+    return;
+  }
+  // U+007B LEFT CURLY BRACKET ({)
+  else if ('{') {
+    return;
+  }
+  // U+007D RIGHT CURLY BRACKET (})
+  else if ('}') {
+    return;
+  }
+  // digit
+  else if ('0' <= c && c <= '9') {
+    return;
+  }
+  // EOF
+  else if ('\0') {
+    return;
+  }
+  // delim
+  else {
+  }
+}  // namespace Flash
 
 }  // namespace Flash
