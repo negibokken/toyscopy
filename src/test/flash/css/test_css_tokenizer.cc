37c0fe145eed45796bf0fbe2f2617fc378be6471
negibokken <negibokken@gmail.com>
2021-01-01 17:48:35 +0900

    Add test case

--- src/test/flash/css/test_css_tokenizer.cc
+++ src/test/flash/css/test_css_tokenizer.cc
@@ -60,3 +60,39 @@ TEST(CSSTokenizerTest, ParseSimpleStyle) {
   }
   EXPECT_EQ(cnt, expected.size());
 }
+
+TEST(CSSTokenizerTest, ParseSimpleStyleWithManySpaces) {
+  Flash::CSSTokenizer* t =
+      new Flash::CSSTokenizer(" span {       color: #ff0000;    }");
+  const std::vector<std::pair<Flash::CSSToken::CSSTokenType, std::string>>
+      expected = {
+          {Flash::CSSToken::WhitespaceToken, " "},
+          {Flash::CSSToken::IdentToken, "span"},
+          {Flash::CSSToken::WhitespaceToken, " "},
+          {Flash::CSSToken::LeftCurlyBracketToken, "{"},
+          {Flash::CSSToken::WhitespaceToken, " "},
+          {Flash::CSSToken::IdentToken, "color"},
+          {Flash::CSSToken::ColonToken, ":"},
+          {Flash::CSSToken::WhitespaceToken, " "},
+          {Flash::CSSToken::HashToken, "#ff0000"},
+          {Flash::CSSToken::SemicolonToken, ";"},
+          {Flash::CSSToken::WhitespaceToken, " "},
+          {Flash::CSSToken::RightCurlyBracketToken, "}"},
+          {Flash::CSSToken::EOFToken, "\0"},
+      };
+
+  // parse all
+  while (t->pumpToken())
+    ;
+
+  int cnt = 0;
+  while (t->canTakeNextToken()) {
+    ASSERT_TRUE(cnt < expected.size());
+    Flash::CSSToken* token = t->nextToken();
+    t->consumeToken();
+    ASSERT_EQ(token->getTokenType(), expected[cnt].first);
+    ASSERT_STREQ(token->getValue().c_str(), expected[cnt].second.c_str());
+    cnt++;
+  }
+  EXPECT_EQ(cnt, expected.size());
+}
